b9a3fef4a8c72afa75be7d25c6831c9345af3df9
xpalau <contact.palau@gmail.com>
2018-02-13 20:47:13 +0100

    Second Initial commit
    Forgot to commit these lines !! This is the first working version !!

--- battleship.py
+++ battleship.py
@@ -1,9 +1,22 @@
 from random import randint
 
+board = []
 
 for x in range(5):
   board.append(["O"] * 5)
 
+def print_board(board):
+  for row in board:
+    print " ".join(row)
+
+#print_board(board)
+
+def random_row(board):
+  return randint(0, len(board) - 1)
+
+def random_col(board):
+  return randint(0, len(board[0]) - 1)
+  
 print board
 
 ship_row = random_row(board)
