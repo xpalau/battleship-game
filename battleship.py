be1fd329846bd5cf64fae88c1ce9636358d32db2
xpalau <contact.palau@gmail.com>
2018-02-13 20:53:40 +0100

    Commit Oscar 1
    Improving a little bit the code.

--- battleship.py
+++ battleship.py
@@ -2,14 +2,19 @@ from random import randint
 
 board = []
 
-for x in range(5):
-  board.append(["O"] * 5)
+
+
+n_rows = 7
+n_cols = 5
+
+for x in range(n_rows):
+  board.append(["O"] * n_cols)
 
 def print_board(board):
   for row in board:
     print " ".join(row)
 
-#print_board(board)
+print_board(board)
 
 def random_row(board):
   return randint(0, len(board) - 1)
@@ -17,7 +22,6 @@ def random_row(board):
 def random_col(board):
   return randint(0, len(board[0]) - 1)
   
-print board
 
 ship_row = random_row(board)
 ship_col = random_col(board)
@@ -39,7 +43,9 @@ for turn in range(4):
     print "Congratulations! You sunk my battleship!"
     break
   else:
-    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+    position_row_out = (guess_row < 0 or guess_row > n_rows - 1)
+    position_col_out = (guess_col < 0 or guess_col > n_cols - 1)
+    if position_row_out or position_col_out:
       print "Oops, that's not even in the ocean."
     elif(board[guess_row][guess_col] == "X"):
       print "You guessed that one already."
